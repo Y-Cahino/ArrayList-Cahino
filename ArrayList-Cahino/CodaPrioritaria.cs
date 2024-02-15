e6541f3f2c6739f80a8383c10f6803455de59364
cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it <cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it>
2024-02-15 10:50:32 +0100

    funzione EstraiPrimo, processo inverso per funzione EstraiUltimo

--- ArrayList-Cahino/CodaPrioritaria.cs
+++ ArrayList-Cahino/CodaPrioritaria.cs
@@ -10,18 +10,34 @@ namespace ArrayList_Cahino
     class CodaPrioritaria
     {
         private ArrayList list;
+        //Costruttore
         public CodaPrioritaria()
         {
             list= new ArrayList();
         }
         public void Add(int id)
         {
+            //Aggiunge l'elemento e riordina
             list.Add(id);
             list.Sort();
         }
-        public void Search(int id)
+        //Cerca elemento nella coda prioritaria
+        public bool Search(int id)
         {
             return list.Contains(id);
+        }
+        public void EstraiPrimo(int id)
+        {
+            if(!IsEmpty())
+            {
+                int primo=
+            }
+        }
+        //Verifica se è vuota
+        public bool IsEmpty()
+        {
+            //True se è vuota
+            return list.Count == 0;
         }
     }
 }
