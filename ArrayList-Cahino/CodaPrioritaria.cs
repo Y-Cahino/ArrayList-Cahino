7f9882362e55d4abe678cbe4fd56d89db22f3a25
cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it <cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it>
2024-02-15 10:40:11 +0100

    funzioni Add, Search

---
+++ ArrayList-Cahino/CodaPrioritaria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Cahino
+{
+    class CodaPrioritaria
+    {
+        private ArrayList list;
+        public CodaPrioritaria()
+        {
+            list= new ArrayList();
+        }
+        public void Add(int id)
+        {
+            list.Add(id);
+            list.Sort();
+        }
+        public void Search(int id)
+        {
+            return list.Contains(id);
+        }
+    }
+}
